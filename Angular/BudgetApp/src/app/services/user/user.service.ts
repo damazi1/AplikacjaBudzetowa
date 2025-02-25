804193d206d4931ad9d6ba2b28e81402c1746747
damazi <dawid.ziora79@gmail.com>
2025-02-25 17:37:41 +0100

    angular

---
+++ Angular/BudgetApp/src/app/services/user/user.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl = 'http://localhost:8080/list';
+  constructor(private  httpClient: HttpClient) { }
+}
